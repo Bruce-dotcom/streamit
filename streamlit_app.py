b1d58bf449abf55dbdb302d2a1470773e3075858
Bruce-dotcom <bruceallen501@gmail.com>
2025-03-04 11:49:22 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,15 @@ st.title("Simple Calculator")
 num1 = st.number_input("Enter the first number:",value=0.0)
 num2 = st.number_input("Enter the second number:",value=0.0)
 
-operation = st.selectbox("Select operation: ",["Addition","Multiplication","Division"])
+operation = st.selectbox("Select operation: ",["Addition","Substraction","Multiplication","Division"])
 
+if st.button("Calculate"):
+    if operation =="Addition":
+        result = num1+num2
+    elif operation == "Substraction":
+        result = num1-num2
+    elif operation == "Multipplication":
+        result = num1*num2
+    elif operation == "Division":
+        result = num1/num2
+    st.success(f"Result:{result}")
