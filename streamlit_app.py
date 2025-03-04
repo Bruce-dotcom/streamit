538471d2bc43fd474179edc10c05225a53375eeb
Bruce-dotcom <bruceallen501@gmail.com>
2025-03-04 11:33:10 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Title
+st.title("Simple Calculator")
+
+
+# Display message
+num1 = st.number_input("Enter the first number:",value=0.0)
+num2 = st.number_input("Enter the second number:",value=0.0)
+
