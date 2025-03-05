e242b30e8fffc08d983841f514af33087ebe43ab
Bruce-dotcom <bruceallen501@gmail.com>
2025-03-05 19:11:12 +0200

    Add files via upload

--- Portfolio/student_portfolio.py
+++ Portfolio/student_portfolio.py
@@ -1,46 +1,103 @@
 import streamlit as st
 
-# Set the Page Title
-st.set_page_config(page_title="Student Portfolio", page_icon="🎓", layout="wide")
+#Set page title and icon
 
-# Side navigation
+st.set_page_config(page_title="Student Portfolio",page_icon="🎓")
+
+#Sidebar naviagtion
+
 st.sidebar.title("📌 Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Projects", "Skills", "Customize Profile", "Contact"])
+page = st.sidebar.radio("Go To:",
+	   ["Home","Projects","Skills","Settings","Contact"])
+#Home section
 
-# Home section
-if page == "Home":
-    st.title("Student Portfolio")
+if page =="Home":
+	st.title("🎓 Student Portfolio")
 
-    # Profile picture upload
-    uploaded_image = st.file_uploader("Upload profile picture", type=["jpg", "png"])
-    if uploaded_image:
-        st.image(uploaded_image, width=150, caption="Profile Picture")
-    else:
-        st.image("Portfolio/person.jpg", width=150, caption="Default Profile Picture")
+	#Profile image
+	uploaded_image = st.file_uploader("Uplaod Profile Picture", type=["jpg","png"])
+	if uploaded_image is not None:
+		st.image(uploaded_image, width=150, caption="Uploaded image")
+	else:
+		st.image("Portfolio/person.jpg", width=150, caption="Default image")
+	#Student details (Editable!)
+	name = st.text_input("Name: ", "ISHIMWE Bruce")
+	location = st.text_input("Location: ", "Musanze,Rwanda")
+	field_of_study = st.text_input("Field of Study: ",
+	                               "Computer Science, SWE")
+	university = st.text_input("University: ", "INES - Ruhengeri")
 
-    # Student details
-    name = st.text_input("Your Name", "ISHIMWE Bruce")
-    location = st.text_input("Location", "Musanze, Rwanda")
-    field_of_study = st.text_input("Field of Study", "Computer Science")
-    university = st.text_input("University", "INES-Ruhengeri")
+	st.write(f"📍{location}")
+	st.write(f"📚{field_of_study}")
+	st.write(f"🎓{university}")
 
-    # Display details
-    st.write(f"📍 {location}")
-    st.write(f"📚 {field_of_study}")
-    st.write(f"🏫 {university}")
+	# Resume download button
+	with open("Portfolio/CV-BRUCE ISHIMWE.pdf", "rb") as file:
+		resume_bytes = file.read()
+	st.download_button(label="📄Download Resume",
+		data=resume_bytes,file_name="CV-BRUCE ISHIMWE.pdf",
+		mime="application/pdf")
 
-#Download resume
-with open("Portfolio/CV-BRUCE ISHIMWE.pdf","rb") as file:
-    resume_bytes = file.read()
-st.download_button(label="📄, Download Resume", data=resume_bytes, file_name="CV-BRUCE ISHIMWE.pdf",mime="application/pdf")
+	st.markdown("---")
+	st.subheader("About Me")
+	about_me = st.text_area("Short introduction about myself:",
+		"I am a passionate AI look forward engineer!")
+	st.write(about_me)
 
-st.markdown("-------------")
-st.subheader("About Me")
-about_me == st.text_area("Write a short description about yourself:","I am a passionate AI Student")
+#Projects section
+elif page =="Projects":
+	st.title("💻 My Projects")
 
-st.write(about_me)
+	with st.expander("📊 Data Analysis Project"):
+		st.write("A project analyzing trends of Rwanda GDP accounts using Pandas and Matplotlib")
 
-#Projects page
-elif page== "Projects":
-    st.title("💻 My Projects")
+	with st.expander("🤖 AI Chatbot"):
+		st.write("Developed an AI-Powered chatbot using Python and NLP Techniques")
 
+	with st.expander("🌐 Caritas CDJP Gikongoro Website"):
+		st.write("Designed and Developed and website for Caritas Gikondoro using Wordpress CMS")
+
+elif page =="Skills":
+	st.title("⚡ Skills and Achievements")
+
+	st.subheader("Programming Skills")
+	skill_python = st.slider("Python",0,100,90)
+	st.progress(skill_python)
+
+	skill_js = st.slider("JavaScript",0,100,75)
+	st.progress(skill_js)
+	skill_AI = st.slider("Artificial Intelligence",0,100,65)
+	st.progress(skill_AI)
+
+	st.subheader("Cerfications & Achievements")
+	st.write("✔ Completed AI&ML in Business Cerfication")
+	st.write("✔ Certified AI in Research and Course Preparation for Education")
+
+elif page == "Settings":
+	st.title("🎨 Customize your profile")
+
+	st.subheader("Upload a Profile Picture")
+	uploaded_image = st.file_uploader("Choose a file", type=["jpg","png"])
+	if uploaded_image:
+		st.image(uploaded_image, width=150)
+
+	st.subheader("✍ Edit Personal Info")
+
+elif page =="Contact":
+	st.title("📬 Contact Me")
+
+	with st.form("contact_form"):
+		name = st.text_input("Your Name")
+		email = st.text_input("Your Email")
+		message=st.text_area("Your message")
+
+		submitted = st.form_submit_button("Send Message")
+		if submitted:
+			st.success("✅ Message sent successfully")
+
+		st.write("📧 Email: mclement@ines.ac.rw")
+		st.write("[🔗LinkedIn](https://ikigugugroup.rw)")
+		st.write("[📂GitHub](https://ikigugugroup.rw)")
+
+	st.sidebar.write("---")
+	st.sidebar.write("🔹 Made with ❤ using My Watermelon")
