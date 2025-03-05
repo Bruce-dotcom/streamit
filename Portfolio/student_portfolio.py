0b33767d9269e5ebb0b50ecf022d1b7ce58d859a
Bruce-dotcom <bruceallen501@gmail.com>
2025-03-05 10:30:24 +0200

    Add files via upload

--- Portfolio/student_portfolio.py
+++ Portfolio/student_portfolio.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="Student Portfolio", page_icon="🎓",layout="wide")
 
 #side navigation
-st.siderbar.title("📌 Navigation")
+st.sidebar.title("📌 Navigation")
 page = st.siderbar.radio("Go to", ["Home","Projects","skills","customize profile","Contact"])
 
 #Home section
@@ -17,3 +17,12 @@ if page == "Home":
         st.image(uploaded_image, width=150,caption="Profile Picture")
     else:
         st.image("person.jpg", width=150,caption="Default Profile Picture")
+#student details
+name = st.text_input("Your Name","ISHIMWE Bruce")
+location = st.text_input("Location", "Musanze,Rwanda")
+field_of_study = st.text_input("Field of study", "Computer Science")
+university = st.text_input("university","INES-Ruhengeri")
+
+st.write(f"📍 {location}")
+st.write(f"📚 {field_of_study}")
+st.write(f"🏫 {university}")
