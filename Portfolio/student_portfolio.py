52cd6208f22f94b6ffd36fe8236f1122dde9b4ba
Bruce-dotcom <bruceallen501@gmail.com>
2025-03-05 10:34:07 +0200

    Add files via upload

--- Portfolio/student_portfolio.py
+++ Portfolio/student_portfolio.py
@@ -1,28 +1,30 @@
 import streamlit as st
 
-#Set the Page Title
-st.set_page_config(page_title="Student Portfolio", page_icon="🎓",layout="wide")
+# Set the Page Title
+st.set_page_config(page_title="Student Portfolio", page_icon="🎓", layout="wide")
 
-#side navigation
+# Side navigation
 st.sidebar.title("📌 Navigation")
-page = st.sidebar.radio("Go to", ["Home","Projects","skills","customize profile","Contact"])
+page = st.sidebar.radio("Go to", ["Home", "Projects", "Skills", "Customize Profile", "Contact"])
 
-#Home section
+# Home section
 if page == "Home":
-    st.title (" Student Portfolio")
+    st.title("Student Portfolio")
 
-    #Profile picture upload
-    uploaded_image = st.file_uploader("Upload profile picture", type["jpg","png"])
+    # Profile picture upload
+    uploaded_image = st.file_uploader("Upload profile picture", type=["jpg", "png"])
     if uploaded_image:
-        st.image(uploaded_image, width=150,caption="Profile Picture")
+        st.image(uploaded_image, width=150, caption="Profile Picture")
     else:
-        st.image("person.jpg", width=150,caption="Default Profile Picture")
-#student details
-name = st.text_input("Your Name","ISHIMWE Bruce")
-location = st.text_input("Location", "Musanze,Rwanda")
-field_of_study = st.text_input("Field of study", "Computer Science")
-university = st.text_input("university","INES-Ruhengeri")
+        st.image("person.jpg", width=150, caption="Default Profile Picture")
 
-st.write(f"📍 {location}")
-st.write(f"📚 {field_of_study}")
-st.write(f"🏫 {university}")
+    # Student details
+    name = st.text_input("Your Name", "ISHIMWE Bruce")
+    location = st.text_input("Location", "Musanze, Rwanda")
+    field_of_study = st.text_input("Field of Study", "Computer Science")
+    university = st.text_input("University", "INES-Ruhengeri")
+
+    # Display details
+    st.write(f"📍 {location}")
+    st.write(f"📚 {field_of_study}")
+    st.write(f"🏫 {university}")
